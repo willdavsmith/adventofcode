97e22ada401221d58cb705a16a5737af3b709c28
willdavsmith <willdavsmith@gmail.com>
2019-12-09 14:34:44 -0500

    updated day 1 solution

--- day1/day1.go
+++ day1/day1.go
@@ -8,6 +8,7 @@ import (
 )
 
 func main() {
+  fmt.Println("===== Part 1 =====")
   file, err := os.Open("data.txt")
   if err != nil {
     panic(err)
@@ -23,6 +24,29 @@ func main() {
       panic(err)
     }
     sum += checkFuelRequired(scanInt)
+  }
+  fmt.Println(sum)
+
+  fmt.Println("===== Part 2 =====")
+  file, err = os.Open("data.txt")
+  if err != nil {
+    panic(err)
+  }
+  defer file.Close()
+
+  scanner = bufio.NewScanner(file)
+
+  sum = 0
+  for scanner.Scan() {
+    scanInt, err := strconv.Atoi(scanner.Text())
+    if err != nil {
+      panic(err)
+    }
+    fuelWeight := checkFuelRequired(scanInt)
+    for fuelWeight > 0 {
+      sum += fuelWeight
+      fuelWeight = checkFuelRequired(fuelWeight)
+    }
   }
   fmt.Println(sum)
 }
